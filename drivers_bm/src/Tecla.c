603379974039863c7fd3ee04fd10697c01cad028
Graciela <gbaena@gmail.com>
2015-09-24 12:46:09 -0300

    Version Inicial de drivers

---
+++ drivers_bm/src/Tecla.c
@@ -0,0 +1,140 @@
+/* Copyright 2015, Eduardo Filomena, Juan Manuel Reta
+ * All rights reserved.
+ *
+ * This file is part of CIAA Firmware.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+/** \brief Blinking Bare Metal example source file
+ **
+ ** This is a mini example of the CIAA Firmware.
+ **
+ **/
+
+/** \addtogroup CIAA_Firmware CIAA Firmware
+ ** @{ */
+
+/** \addtogroup Examples CIAA Firmware Examples
+ ** @{ */
+/** \addtogroup Baremetal Bare Metal example source file
+ ** @{ */
+
+/*
+ * Initials     Name
+ * ---------------------------
+ *
+ */
+
+/*
+ * modification history (new versions first)
+ * -----------------------------------------------------------
+ * yyyymmdd v0.0.1 initials initial version
+ */
+
+/*==================[inclusions]=============================================*/
+#include "Tecla.h"       /* <= own header */
+
+
+
+
+/*==================[macros and definitions]=================================*/
+
+/*==================[internal data declaration]==============================*/
+
+/*==================[internal functions declaration]=========================*/
+
+/*==================[internal data definition]===============================*/
+
+/*==================[external data definition]===============================*/
+
+/*==================[internal functions definition]==========================*/
+
+/*==================[external functions definition]==========================*/
+/** \brief Main function
+ *
+ * This is the main entry point of the software.
+ *
+ * \returns 0
+ *
+ * \remarks This function never returns. Return value is only to avoid compiler
+ *          warnings or errors.
+ */
+
+ void InicializarTecla(void)
+ {
+Chip_GPIO_Init(LPC_GPIO_PORT);
+Chip_SCU_PinMux(1,0,MD_PUP|MD_EZI|MD_ZI,FUNC0);
+Chip_SCU_PinMux(1,1,MD_PUP|MD_EZI|MD_ZI,FUNC0);
+Chip_SCU_PinMux(1,2,MD_PUP|MD_EZI|MD_ZI,FUNC0);
+Chip_SCU_PinMux(1,6,MD_PUP|MD_EZI|MD_ZI,FUNC0);
+
+Chip_GPIO_SetDir(LPC_GPIO_PORT,0,1<<4,0);   //10000000000000
+Chip_GPIO_SetDir(LPC_GPIO_PORT,0,1<<8,0);
+Chip_GPIO_SetDir(LPC_GPIO_PORT,0,1<<9,0);
+Chip_GPIO_SetDir(LPC_GPIO_PORT,1,1<<9,0);
+
+
+
+ }
+
+ uint8_t DetectarTecla1(void)
+
+ {
+	 Chip_GPIO_ReadPortBit(LPC_GPIO_PORT, 0, 4);
+ }
+
+ uint8_t DetectarTecla2(void)
+ {
+	 Chip_GPIO_ReadPortBit(LPC_GPIO_PORT, 0, 8);
+ }
+
+ uint8_t DetectarTecla3(void)
+ {
+	 Chip_GPIO_ReadPortBit(LPC_GPIO_PORT, 0, 9);
+ }
+ uint8_t DetectarTecla4(void)
+ {
+	 Chip_GPIO_ReadPortBit(LPC_GPIO_PORT, 1, 9);
+ }
+
+
+
+
+
+
+
+
+
+
+
+/** @} doxygen end group definition */
+/** @} doxygen end group definition */
+/** @} doxygen end group definition */
+/*==================[end of file]============================================*/
+
